864bb491cc6cea5f42a2415126c4eebcdf5b85f0
XY <3400164864@qq.com>
2023-04-23 17:08:49 +0800

    chore: 给useDebounce添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,7 +35,7 @@ export const useMount = (callback: () => void) => {
 //   }
 // }
 
-export const useDebounce = (value: unknown, delay?: number): any => {
+export const useDebounce = <V>(value: V, delay?: number): V => {
   const [debounceValue, setDebouncedValue] = useState(value)
   useEffect(() => {
     // 每次在value变化以后设置一个定时器
