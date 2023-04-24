776db37951f8bfb6512f22294d550b7e3c87eae7
XY <3400164864@qq.com>
2023-04-24 10:35:14 +0800

    chore: 自定义登录接口

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,36 @@
+import React, { FormEvent } from 'react'
+
+const apiUrl = 'http://localhost:3001'
+
+export const LoginScreen = () => {
+  const login = (params: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(params)
+    }).then(async (response) => {})
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    // 阻止表单提交的默认行为
+    event.preventDefault()
+    const username = (event.currentTarget.elements[0] as HTMLInputElement).value
+    const password = (event.currentTarget.elements[1] as HTMLInputElement).value
+    login({ username, password })
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" placeholder="请输入用户名" id={'username'} />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id={'userpassword'} placeholder="请输入密码" />
+      </div>
+      <button type={'submit'}>登录</button>
+    </form>
+  )
+}
